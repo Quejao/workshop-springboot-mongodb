c0f519d573c78fb068c44451eca4d93cd7ebbc0a
Quejao <leotrone@hotmail.com>
2022-05-17 10:21:25 -0300

    Inserindo getters e setters do readerDTO

--- src/main/java/com/leocptr/workshopmongo/dto/ReaderDTO.java
+++ src/main/java/com/leocptr/workshopmongo/dto/ReaderDTO.java
@@ -17,4 +17,20 @@ public class ReaderDTO implements Serializable {
         this.id = obj.getId();
         this.name = obj.getName();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
