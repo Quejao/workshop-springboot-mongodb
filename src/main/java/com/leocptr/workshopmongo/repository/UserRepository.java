dfcb2b37ee9d3b66160f01e71e773e4da156388c
Quejao <leotrone@hotmail.com>
2022-05-17 08:06:25 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/leocptr/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.leocptr.workshopmongo.repository;
+
+import com.leocptr.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
